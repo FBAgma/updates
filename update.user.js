b7f5bc1091e7958e3dc935c997e9c67a194729ac
FBAgma <fbagmaio@gmail.com>
2024-11-09 18:54:19 +0300

    Create update.user.js

---
+++ update.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Update
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Update
+// @author       FBAgma
+// @match        *://agma.io/*
+// @run-at       document-start
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=agma.io
+// @grant        unsafeWindow
+// @downloadURL https://github.com/FBAgma/updates/update.user.js
+// @updateURL https://github.com/FBAgma/updates/update.user.js
+// ==/UserScript==
