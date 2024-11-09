3dcc2f1e9718f4e3d73416fe0dfb7d6ed34f0daa
FBAgma <fbagmaio@gmail.com>
2024-11-09 19:59:15 +0300

    Update update.user.js

--- update.user.js
+++ update.user.js
@@ -8,6 +8,6 @@
 // @run-at       document-start
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=agma.io
 // @grant        unsafeWindow
-// @downloadURL https://github.com/FBAgma/updates/update.user.js
-// @updateURL https://github.com/FBAgma/updates/update.user.js
+// @downloadURL https://github.com/FBAgma/updates/raw/refs/heads/main/update.user.js
+// @updateURL https://github.com/FBAgma/updates/raw/refs/heads/main/update.user.js
 // ==/UserScript==
